b7b2b357a4076a42f45b737db47ceca8d84354e1
SuperKode <trym_ab@hotmail.com>
2016-01-21 13:00:15 +0100

    Create Exc_2.py

---
+++ Exc_2.py
@@ -0,0 +1,38 @@
+# TTK4145
+# Exc_1
+
+from threading import Thread, Lock
+
+mtx = Lock()
+i = 0
+
+# Function for increasing i
+def increaseNumber():
+  mtx.aquire()
+	  global i
+	  for x in range(0, 999998)
+		  i += 1
+	mtx.release()
+
+# Function for decreasing i
+def decreaseNumber():
+  mtx.aquire()
+    global i
+	  for x in range(0, 999999)
+		  i -= 1
+	mtx.release()
+
+# Main function
+main:
+    threadIncNumber = Thread(target = increaseNumber, args = (),)
+    threadIncNumber.start()
+    
+	  threadDecNumber = Thread(target = decreaseNumber, args = (),)
+    threadDecNumber.start()
+    
+	  threadIncNumber.join()
+    threadDecNumber.join()
+	
+    print(i)
+	
+main()
